4dda2a82a97efe15400f4ab4b3d7ed84266077de
Tsakuya1625 <167065507+Tsakuya1625@users.noreply.github.com>
2024-07-16 01:40:49 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <heade>
+        <meta charset="UFT-8">
+        <title>kadai_007</title>
+
+    </heade>
+
+    <body>
+        <p>
+            <?php
+                $array = ['name' => 'onion','price' => 200,'weight' => 160];
+                
+                print_r($array);
+            ?>
+        </p>
+    </body>
+
+</html>
